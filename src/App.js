71154fab415c5e516dad16bbe1df1ee167ff3c98
Manolo Perez <manolope338@gmail.com>
2019-05-30 12:20:01 -0500

    Primera prueba de conexión a server. Exitosa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { TransitoClient } = require('./crashify_grpc_web_pb.js');
+const { Sesion, Usuario } = require('./crashify_pb.js');
+
+var client = new TransitoClient('http://localhost:8080', null, null);
+
+class App extends Component {
+  
+  callGrpcService = () => {
+    const sesion = new Sesion();
+    sesion.setUsuario('deklok');
+    sesion.setPassword('123456');
+
+    client.iniciarSesion(sesion, {}, (err, usuario) => {
+      if (usuario == null) {
+        console.log(err)
+      }else {
+        console.log(usuario.getNombre())
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button style={{padding:10}} onClick={this.callGrpcService}>Click for grpc request</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
